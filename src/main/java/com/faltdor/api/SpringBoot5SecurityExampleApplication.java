a37b9c1b53f60012720051db17a73baa7b665895
faltdor <faltdor@gmail.com>
2017-10-18 17:26:05 -0400

    first commit

---
+++ src/main/java/com/faltdor/api/SpringBoot5SecurityExampleApplication.java
@@ -0,0 +1,12 @@
+package com.faltdor.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot5SecurityExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot5SecurityExampleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/faltdor/api/SpringBoot5SecurityExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.faltdor.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBoot5SecurityExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
